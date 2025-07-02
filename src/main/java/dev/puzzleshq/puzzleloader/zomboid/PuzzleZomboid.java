44780ab9dd940ae611f34c4d98f8e6210481b290
Mr-Zombii <thezombiigames@gmail.com>
2025-07-01 19:57:50 -0500

    added SteamAppLocator (from zombi2),comment out mixins, made loader launchable, added jar maker for project zomboid

--- src/main/java/dev/puzzleshq/puzzleloader/zomboid/PuzzleZomboid.java
+++ src/main/java/dev/puzzleshq/puzzleloader/zomboid/PuzzleZomboid.java
@@ -5,7 +5,6 @@ import dev.puzzleshq.puzzleloader.zomboid.entrypoints.PostInit;
 import dev.puzzleshq.puzzleloader.zomboid.entrypoints.PreModInit;
 
 public class PuzzleZomboid implements PreModInit, ModInit, PostInit {
-
     @Override
     public void onInit() {
 
